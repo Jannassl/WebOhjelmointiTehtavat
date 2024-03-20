202618bf1a57447e51bc30485c258c27b5c304c1
Jannassl <Jan.nassling@metropolia.fi>
2024-03-20 13:18:44 +0200

    Task 3 tehty

--- viikko2/JavaRecap1/Task3/JS.js
+++ viikko2/JavaRecap1/Task3/JS.js
@@ -1,2 +1,17 @@
 'use strict'
 
+let s1 = parseInt(prompt("Anna 1.sivun pituus"))
+let s2 = parseInt(prompt("Anna 2.sivun pituus"))
+let s3 = parseInt(prompt("Anna 3.sivun pituus"))
+
+let kolmionTyyppi;
+
+if (s1 === s2 && s2 === s3) {
+  kolmionTyyppi = "Equilateral";
+} else if (s1 === s2 || s1 === s3 || s2 === s3) {
+  kolmionTyyppi = "Isosceles";
+} else {
+  kolmionTyyppi = "Scalene";
+}
+
+document.querySelector('#result').innerHTML = "kolmion tyyppi on: " + kolmionTyyppi;
