2dac7aed8ecc8bd63fbd66c0eb1a57443004abc1
Jannassl <Jan.nassling@metropolia.fi>
2024-03-20 13:06:25 +0200

    Update

--- viikko2/JavaRecap1/Task1/JS.js
+++ viikko2/JavaRecap1/Task1/JS.js
@@ -2,5 +2,7 @@
 
 const c = prompt("Anna asteet")
 const  f = (c * 9/5) + 32
+const k = parseFloat(c) + 273.15
 
 document.querySelector('#target1').innerHTML = "Asteet muutettu c -> "+ f 
+document.querySelector('#target2').innerHTML = "Asteet muutettu c -> "+ k 
