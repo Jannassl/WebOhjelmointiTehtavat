f5b0709e5a78b164674587f9f4e532bb4ff92071
Jannassl <Jan.nassling@metropolia.fi>
2024-03-21 15:15:37 +0200

    zippi file

---
+++ viikko2/JavaRecap3/dom-assignments/dom-assignments/t1/t1.js
@@ -0,0 +1,40 @@
+// array for todo list
+const todoList = [
+  {
+    id: 1,
+    task: 'Learn HTML',
+    completed: true,
+  },
+  {
+    id: 2,
+    task: 'Learn CSS',
+    completed: true,
+  },
+  {
+    id: 3,
+    task: 'Learn JS',
+    completed: false,
+  },
+  {
+    id: 4,
+    task: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: 5,
+    task: 'Learn React',
+    completed: false,
+  },
+];
+
+
+let ulElement = document.querySelector('ul');
+
+
+for(let todo of todoList){
+
+    let listItem = `<li>${todo.task} - ${todo.completed ? 'Completed' : 'Not Completed'}</li>`;
+
+    ulElement.insertAdjacentHTML('beforeend', listItem);
+}
+
