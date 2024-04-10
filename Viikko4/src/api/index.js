612c87b5155f0abc9d643d76825d5061f34f4210
Jannassl <Jan.nassling@metropolia.fi>
2024-04-10 11:08:38 +0300

    Assignment2 updates

--- Viikko4/src/api/index.js
+++ Viikko4/src/api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import catRouter from './routes/cat-router.js';
+import userRouter from './routes/user-router.js';
 
 const router = express.Router();
 
 // bind base url for all cat routes to catRouter
-router.use('/cats', catRouter);
-
+//router.use('/cats', catRouter);
+router.use('/user', userRouter);
 export default router;
--- Viikko4/src/api/models/user.model.js
+++ Viikko4/src/api/models/user.model.js
@@ -8,7 +8,7 @@ const userItems = [
      password: 'password',
   },
   {
-    user_id: 3609,
+    user_id: 3610,
     name: 'Matti Doe',
     username: 'Matidoe',
     email: 'matti@metropolia.fi',
@@ -16,7 +16,7 @@ const userItems = [
     password: 'password',
  },
  {
-  user_id: 3609,
+  user_id: 3611,
   name: 'Pekka Doe',
   username: 'pekadoe',
   email: 'pekka@metropolia.fi',
@@ -33,9 +33,11 @@ const findUserById = (id) => {
   return userItems.find((item) => item.user_id == id);
 };
 
-const assUser = (user) => {
+const addUser = (user) => {
   const {name, username, email, role, password}=user;
   const newId = userItems[0].user_id +1;
   userItems.unshift({user_id: newId, name, username, email, role, password});
   return {user_id: newId};
 };
+
+export {listAllUsers, findUserById, addUser};
